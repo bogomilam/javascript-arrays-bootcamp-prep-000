a2f36a8505849bac98473155b468494235959351
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-26 19:49:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  return  chocolateBars.unshift("mars")
+  return  array.unshift("element")
 }
 
 function destructivelyAddElementToBeginningOfArray(arguments,element) {
