243cbb59f0a81e717b93181731d27dd47a0aac06
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-26 20:09:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,8 +15,7 @@ function addElementToEndOfArray(array, element) {
   return [...array,element]
 }
 
-function addElementToEndOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
 array.push(element)
 return array
-}
 }
