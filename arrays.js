f68e5ce37e2f09a135147316a4775a77742f4809
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-26 20:00:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,9 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 
+
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return array.unshift(element)
+array.unshift(element)
+  return 
+  
 }
